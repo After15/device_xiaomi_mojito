ff0a08cb25ed16b687c1bfea25b6c88d2e4b2c35
Sachinpawar86 <sachinbarange86@gmail.com>
2024-12-07 06:08:14 +0000

    mojito : bringup for afterlife

--- afterlife_mojito.mk
+++ afterlife_mojito.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some LineageOS stuffs
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some AfterlifeOS stuffs
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_mojito
+PRODUCT_NAME := afterlife_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
